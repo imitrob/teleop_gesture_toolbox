3e3203ba4f3bb9a07d1f5dbad64aff4891141333
Petr Vanc <petr.vanc@cvut.cz>
2026-01-21 09:59:51 +0100

    qos for HandListener

--- gesture_detector/gesture_detector/hand_processing/hand_listener.py
+++ gesture_detector/gesture_detector/hand_processing/hand_listener.py
@@ -4,11 +4,18 @@ import collections
 import gesture_msgs.msg as rosm
 from gesture_detector.hand_processing.frame_lib import Frame
 
+from rclpy.qos import QoSProfile, HistoryPolicy, ReliabilityPolicy
+
 class HandListener():
     def __init__(self):
         super(HandListener, self).__init__()
         self.hand_frames = collections.deque(maxlen=10)
-        self.create_subscription(rosm.Frame, '/teleop_gesture_toolbox/hand_frame', self.hand_frame_callback, 10)
+        qos = QoSProfile(
+            history=HistoryPolicy.KEEP_LAST,
+            depth=1,
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+        )
+        self.create_subscription(rosm.Frame, '/teleop_gesture_toolbox/hand_frame', self.hand_frame_callback, qos)
         print("Note: `You need ros2 run gesture_detector leap` and `sudo leapd` running", flush=True)
 
     def hand_frame_callback(self, data):
